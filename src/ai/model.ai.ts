960b5c972fd5547b704ae06dcfb5e08f583cf25a
Jayprakash Narayan <jprakashlucky05@gmail.com>
2026-09-01 13:20:33 +0530

    Judge Working fine

--- src/ai/model.ai.ts
+++ src/ai/model.ai.ts
@@ -4,7 +4,7 @@ import { ChatCohere } from "@langchain/cohere";
 import config from "../config/config.js"
 
 export const geminiModel = new ChatGoogle({
-    model: "gemini-flash-latest",
+    model: "gemini-3.6-flash",
     apiKey: config.GOOGLE_API_KEY,
 })
 
--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,14 @@ import graph from "./ai/graph.ai.js"
 const app = express()
 
 
-app.get("/", async (req, res) => {
+app.get("/api", async (req, res) => {
     const result = await graph("Whats the best Ai model for text generation?")
     res.json(result)
     
 })
 
-app.post("/use-graph",async (req,res)=>{
+// app.post("/use-graph",async (req,res)=>{
 
-})
+// })
 
 export default app
